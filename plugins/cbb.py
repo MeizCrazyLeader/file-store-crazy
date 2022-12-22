46c4f6ef365f0632e4c2a8ae04211995415339c4
MeizCrazyLeader <110610631+MeizCrazyLeader@users.noreply.github.com>
2022-12-22 18:42:37 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Owner : <a href='https://telegram.dog/GreyMatter_Owner'>GreyMatter's Owner</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Greymattersbot/File-Sharing-Bot'>Click here</a>\n○ Channel : @GreyMatter_Bots\n○ Support Group : @GreyMatters_Bots_Discussion</b>",
+            text = f"<b>○ Owner : <a href='https://telegram.dog/CrazyLeader_bot'>CrazyLeader Owner</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='NAHI MILEGA :)>Click here</a>\n○ Channel : @ArjunaBatch_2023\n○ Support Group : @Arjuna_JEE_Chat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
